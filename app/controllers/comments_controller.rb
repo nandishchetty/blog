0ef64c5fcc3c1824815876fc129300f1f9f38d39
imnithin <vayu@Vayus-iMac.local>
2017-07-31 18:21:17 +0530

    first commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,18 @@
+ class CommentsController < ApplicationController
+
+
+ 	def show
+ 		@article = Article.find(params[:id])
+ 	end
+
+  def create
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.create(comment_params)
+    redirect_to article_path(@article)
+  end
+ 
+  private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
+end
